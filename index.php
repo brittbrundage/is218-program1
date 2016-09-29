2a28220a8aa459f14683446fdf8a4e680fa67f7b
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-09-29 14:24:48 -0400

    added more code

--- index.php
+++ index.php
@@ -29,5 +29,44 @@
 
 	print_r($myAssoc);
 
+//This is how you print r access a array by naming they key you want to access
+	print_r($myAssoc['value1']);
+
+//This is how you define a class in php
+	class myclass {
+	public $myPublic;
+	private $myPrivate;
+	protected $myProtected;
+
+	public function __construct() {
+
+//when we are working inside the class scametic, property is a equal to a
+//variable outside of a class. 3 different properties they can have.
+//public can be used as properties outside of the class
+//private is only inside the class
+//protected is in between public and private, it is a property that can be 
+//accessed inside classes and subclasses.
+
+	$this->myPublic = 1;
+//$this is the internal reference to the object within a class
+//The arrow points to the preoprty or method being withing the object
+	$this->myPrivate = 2;
+	$this->MyProtected = 3;
+
+//This is how you call a method inside an object and pass a value
+	$this->sayHello('Brittani');
+	}
+
+
+	public function sayHello($name) {
+		echo 'Hello' . $name;
+	}
+
+//This is how you create a new class object
+	$obj = new myclass;
+	print_r($obj);
+
+
+
 
 ?>
