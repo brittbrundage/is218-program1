3449c0448c5cd1b4cf81a23a877c844adf7d3da8
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-09-29 13:37:07 -0400

    added some code for lesson 1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+//This is how you print test in php
 	echo 'hello world';
+//This is how you store a value in a variable
+	$myvar = 'Some Text';
+//This is an example of the difference between single quotes and double quotes
+	echo '<br>';
+	echo '$myvar';
+	echo '<br>';
+	echo "$myvar";
+
 ?>
