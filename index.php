73e0f9deef2fa099d039f256fe8f301e81ffefd2
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-09-29 13:50:07 -0400

    added array info

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
+//Lesson 1
+
 //This is how you print test in php
 	echo 'hello world';
+
 //This is how you store a value in a variable
 	$myvar = 'Some Text';
+
 //This is an example of the difference between single quotes and double quotes
 	echo '<br>';
 	echo '$myvar';
 	echo '<br>';
 	echo "$myvar";
+
+//This is an example of php arrays
+
+	$myarray = array();
+	$myarray[] = 'some value 1';
+	$myarray[] = 'some value 2';
+	$myarray[] = 'some value 3';
+
+//Two ways to print to the screen which is print or echo (most people use echo), print_r is used to see inside of objects or arrays
+	print_r($myarray);
+
+//This is an example of associative array and a nested array
+
+	$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+
+	print_r($myAssoc);
+
 
 ?>
